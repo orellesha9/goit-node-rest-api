fa9f93b29ee363355ea2895017be45df2f31f316
orellesha9 <orellesha9@gmail.com>
2024-03-30 23:14:08 +0200

    finish

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,12 +4,10 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-  favorite: Joi.boolean(),
 });
